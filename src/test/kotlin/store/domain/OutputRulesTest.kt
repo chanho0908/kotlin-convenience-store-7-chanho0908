f66b0a145abb78ca16ef0ba77aa7b94792d69ecf
chanho0908 <uni070@naver.com>
2024-11-07 02:29:44 +0900

    test : 사용자 상품 안내용 메시지 포맷 변환 테스트 추가

---
+++ src/test/kotlin/store/domain/OutputRulesTest.kt
@@ -0,0 +1,24 @@
+package store.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import store.domain.model.output.OutputRules
+
+class OutputRulesTest {
+
+    @Test
+    fun `판매_상품_안내용_텍스트_포맷_변환_테스트`() {
+
+        val actual = OutputRules.productFormat(
+            name = "콜라",
+            price = "1,000",
+            quantity = "10",
+            promotion = "탄산2+1"
+        )
+
+        val expected = "- 콜라 1,000원 10개 탄산2+1"
+
+        assertThat(actual).isEqualTo(expected)
+    }
+
+}
