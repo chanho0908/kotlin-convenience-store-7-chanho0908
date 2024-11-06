77bd61b33f264ec39e76d946fdc24361c90bcf2d
chanho0908 <uni070@naver.com>
2024-11-07 02:15:34 +0900

    refactor : 상품 목록 응답 클래스 수정
    - price : 원화 단위로 변환하도록 수정했습니다.
    - quantity : 재고가 없을 경우 "재고 없음"을 출력하고 재고가 있을 경우 해당 재고를 출력하도록 수정했습니다.
    - promotion : 적용된 프로모션이 없을 경우 빈 문자열, 프로모션이 있을 경우 해당 프로모션을 반환합니다.

--- src/main/kotlin/store/data/dto/ProductResponse.kt
+++ src/main/kotlin/store/data/dto/ProductResponse.kt
@@ -5,22 +5,24 @@ import store.domain.model.product.ProductRules.PRODUCT_PRICE
 import store.domain.model.product.ProductRules.PRODUCT_QUANTITY
 import store.domain.model.product.ProductRules.PRODUCT_PROMOTION
 import store.domain.model.CommonRules.OUT_OF_STOCK
-import store.domain.model.product.Product
-import store.domain.model.product.Products
+import store.domain.model.product.Item
+import java.text.NumberFormat
+import java.util.Locale
 
 data class ProductResponse(
     val name: String,
     val price: Int,
-    val quantity: Int,
+    val quantity: String,
     val promotion: String?
 ) {
-    fun toDomainModel(): Product {
-        return Product(
+    fun toDomainModel(): Item {
+        val item = Item(
             name = name,
-            price = price,
+            price = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price),
             quantity = quantity,
-            promotion = promotion ?: OUT_OF_STOCK.getValue()
+            promotion = promotion
         )
+        return item
     }
 }
 
@@ -33,7 +35,7 @@ fun List<String>.toProductResponse(): ProductResponse {
     return ProductResponse(
         name = this[name],
         price = this[price].toInt(),
-        quantity = this[quantity].toInt(),
-        promotion = if (this[promotion] == "null") null else this[promotion]
+        quantity = if (this[quantity] == "0") OUT_OF_STOCK.toString() else this[quantity],
+        promotion = if (this[promotion] == "null") "" else this[promotion]
     )
 }
