43767212d88deee382c2bb0e8f637963c10866ae
chanho0908 <uni070@naver.com>
2024-11-08 02:59:05 +0900

    test : Data Layer 테스트 데이터 추가

--- src/test/kotlin/store/data/repository/FakeProductRepositoryImpl.kt
+++ src/test/kotlin/store/data/repository/FakeProductRepositoryImpl.kt
@@ -8,7 +8,10 @@ class FakeProductRepositoryImpl : ProductRepository {
     override fun getProduct(): Products {
         val items = listOf(
             ProductItem("콜라", "1,000", "10개", "탄산2+1"),
-            ProductItem("콜라", "1,000", "10개", ""),
+            ProductItem("콜라", "1,000", "재고 없음", ""),
+            ProductItem("사이다", "1,000", "8개", "탄산2+1"),
+            ProductItem("물", "1,000", "10개", ""),
+            ProductItem("오렌지주스", "1,000", "10개", "")
         )
         return Products(items)
     }
