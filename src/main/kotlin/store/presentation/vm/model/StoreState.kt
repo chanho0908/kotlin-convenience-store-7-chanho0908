b492480c07289b08c06742d799c0ef259cb810f6
chanho0908 <uni070@naver.com>
2024-11-07 13:41:18 +0900

    feat : ViewModel에서 상태관리하기 위한 class 추가

---
+++ src/main/kotlin/store/presentation/vm/model/StoreState.kt
@@ -0,0 +1,20 @@
+package store.presentation.vm.model
+
+import store.domain.model.output.OutputRules
+import store.domain.model.product.Products
+import store.domain.model.promotion.Promotions
+import store.presentation.event.UiEvent
+
+data class StoreState(
+    val products: Products,
+    val promotions: Promotions,
+    val uiEvent: UiEvent
+) {
+    companion object {
+        fun create() = StoreState(
+            Products(emptyList()),
+            Promotions(emptyList()),
+            UiEvent.Loading(OutputRules.WELCOME.toString())
+        )
+    }
+}
