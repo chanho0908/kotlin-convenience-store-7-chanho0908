7c0941d1304a2d5d5b53c043b1e9168ae1f25d6f
chanho0908 <uni070@naver.com>
2024-11-08 03:28:08 +0900

    feat : 구분자를 기준으로 문자열을 스플릿 하는 확장함수 추가

--- src/main/kotlin/store/domain/ext/StringExt.kt
+++ src/main/kotlin/store/domain/ext/StringExt.kt
@@ -1,4 +1,11 @@
 package store.domain.ext
 
+import store.domain.model.Constants
 
-fun String.splitByComma(): List<String> = this.split(",")
+fun String.isNumeric(): Boolean {
+    return this.all { it.isDigit() }
+}
+
+fun String.splitByComma(): List<String> = this.split(Constants.COMMA.toString())
+
+fun String.splitByHyphen(): List<String> = this.split(Constants.HYPHEN.toString())
