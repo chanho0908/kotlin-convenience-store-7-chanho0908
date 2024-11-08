f4db931f7a49ea7bc652ab0ff8023cf07cf10982
chanho0908 <uni070@naver.com>
2024-11-09 02:21:29 +0900

    feat : 유효한 프로모션이 있다면 프로모션 상태를 추가하는 기능 추가

--- src/main/kotlin/store/presentation/vm/ViewModel.kt
+++ src/main/kotlin/store/presentation/vm/ViewModel.kt
@@ -3,15 +3,18 @@ package store.presentation.vm
 import store.domain.usecase.CheckOrderValidationUseCase
 import store.domain.repository.ProductRepository
 import store.domain.usecase.ExtractOrdersUseCase
+import store.domain.usecase.GetUnexpiredPromotionUseCase
 import store.presentation.vm.model.StoreState
 import store.presentation.event.UiEvent
+import store.presentation.vm.model.Order
 import store.presentation.vm.model.Orders
+import store.presentation.vm.model.toUiModel
 
 class ViewModel(
     private val productRepository: ProductRepository,
-    private val promotionRepository: PromotionRepository,
     private val checkOrderValidationUseCase: CheckOrderValidationUseCase,
-    private val extractOrdersUseCase: ExtractOrdersUseCase
+    private val extractOrdersUseCase: ExtractOrdersUseCase,
+    private val getUnexpiredPromotionUseCase: GetUnexpiredPromotionUseCase
 ) {
     private var _state = StoreState.create()
     val state get() = _state
@@ -25,6 +28,7 @@ class ViewModel(
         )
     }
 
+    fun orderBuyProduct(order: String) {
         val products = _state.products
         checkOrderValidationUseCase(order = order, products = products)
         onCompleteCheckOrderValidation(order)
@@ -33,11 +37,27 @@ class ViewModel(
     private fun onCompleteCheckOrderValidation(order: String) {
         val extractedOrders = Orders(extractOrdersUseCase(order))
         _state = _state.copy(orders = extractedOrders)
+        getUnexpiredPromotionUseCase()
+    }
+
+    private fun getUnexpiredPromotionUseCase() {
+        checkPromotion()
+    }
 
     private fun checkPromotion(){
         val updateOrders = _state.orders.items.map { order ->
-            order.copy(promotion = _state.products.hasPromotion(order.name))
+            val hasPromotion = _state.products.hasPromotion(order.name)
+            setOrderPromotion(order, hasPromotion)
         }
         _state = _state.copy(orders = Orders(updateOrders))
+    }
+
+    private fun setOrderPromotion(currentOrder: Order, hasPromotion: String?): Order{
+        return if (hasPromotion != null){
+            val unExpiredPromotion = getUnexpiredPromotionUseCase(hasPromotion)
+            currentOrder.copy(promotion = unExpiredPromotion?.toUiModel())
+        }else {
+            currentOrder
+        }
     }
 }
