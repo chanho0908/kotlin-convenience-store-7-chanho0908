58429c225b2e4f164835442012897dfa1f81ac9e
chanho0908 <uni070@naver.com>
2024-11-09 14:50:24 +0900

    refactor : 사용자 입력에서 구매 상품만 추출하는 로직 수정

--- src/main/kotlin/store/domain/usecase/ExtractOrdersUseCase.kt
+++ src/main/kotlin/store/domain/usecase/ExtractOrdersUseCase.kt
@@ -4,19 +4,18 @@ import store.domain.ext.extractProductName
 import store.domain.ext.extractProductQuantity
 import store.domain.ext.splitByComma
 import store.domain.ext.splitByHyphen
-import store.presentation.vm.model.Order
 
 class ExtractOrdersUseCase {
-    operator fun invoke(orderList: String): List<Order> {
+    operator fun invoke(orderList: String): List<Pair<String, Int>> {
         return orderList.splitByComma().map {
             val splitOrder = it.splitByHyphen()
             extractOrder(splitOrder[0], splitOrder[1])
         }
     }
 
-    private fun extractOrder(productName: String, productQuantity: String): Order {
+    private fun extractOrder(productName: String, productQuantity: String): Pair<String, Int> {
         val name = productName.extractProductName()
         val quantity = productQuantity.extractProductQuantity().toInt()
-        return Order(name, quantity, null)
+        return Pair(name, quantity)
     }
 }
