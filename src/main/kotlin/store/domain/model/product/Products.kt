93f92c5bdc620eca72d58a1658eae263dfa75659
chanho0908 <uni070@naver.com>
2024-11-10 02:14:24 +0900

    feat : 상품에 관한 도메인 로직 추가
    isPromotionStockEnough : 프로모션이 적용되는 재고의 수량이 충분한지 판별
    getPrice : 상품의 가격을 정수로 반환해 출력

--- src/main/kotlin/store/domain/model/product/Products.kt
+++ src/main/kotlin/store/domain/model/product/Products.kt
@@ -1,5 +1,6 @@
 package store.domain.model.product
 
+import store.domain.ext.removeStockUnitSuffix
 import store.domain.model.Exception
 import store.domain.model.output.OutputRules
 import store.domain.model.output.OutputRules.Companion.productFormat
@@ -9,16 +10,27 @@ data class Products(
 ) {
     private fun joinToLineBreak() = items.joinToString("\n") { toUiModel(it) }
 
-    fun makeCurrentStockGuideMessage(): String{
+    fun makeCurrentStockGuideMessage(): String {
         return "${this.joinToLineBreak()}\n\n${OutputRules.GUIDE}"
     }
 
-    fun hasPromotion(name: String): String?{
+    fun hasPromotion(name: String): String? {
         return items.find { it.name == name && it.promotion != null }?.promotion
     }
 
+
+    fun isPromotionStockEnough(name: String, quantity: Int): Boolean {
+        val promotionStockQuantity = items.find { it.name == name && it.promotion != null }
+            ?.quantity?.removeStockUnitSuffix()?.toInt() ?: 0
+
+        return promotionStockQuantity >= quantity
+    }
+
     fun getPrice(name: String): String {
-        return items.find { it.name == name }?.price ?: "${Exception.NOT_SALES}"
+        return items.find { it.name == name }
+            ?.price
+            ?.replace(",", "")
+            ?: "${Exception.NOT_SALES}"
     }
 
     private fun toUiModel(product: ProductItem) = productFormat(product)
