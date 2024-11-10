dd8407a848303d6152e16c44b7683bbfab2e213a
chanho0908 <uni070@naver.com>
2024-11-10 23:34:12 +0900

    feat : 추가 프로모션, 재고 출력에 대한 상수 관리 추가

--- src/main/kotlin/store/domain/model/output/OutputRules.kt
+++ src/main/kotlin/store/domain/model/output/OutputRules.kt
@@ -7,7 +7,11 @@ enum class OutputRules(private val msg: String) {
     GUIDE("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
     OUT_OF_STOCK("재고 없음"),
     STOCK_UNIT("개"),
-    PRODUCT("- %s %s원 %s %s");
+    PRODUCT("- %s %s원 %s %s"),
+    NOT_RECEIVED_PROMOTION("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
+    STORAGE_STOCK("현재 %s %s개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
+    YES("Y"),
+    NO("N");
 
     override fun toString(): String = msg
 
@@ -19,6 +23,14 @@ enum class OutputRules(private val msg: String) {
                 product.quantity,
                 product.promotion
             ).trim()
+        }
+
+        fun notReceivedPromotionFormat(name: String): String {
+            return NOT_RECEIVED_PROMOTION.toString().format(name)
+        }
+
+        fun storageStockFormat(name: String, quantity: Int): String {
+            return STORAGE_STOCK.toString().format(name, quantity)
         }
     }
 }
