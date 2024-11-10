2888f48b596bd64eb9ed5fa243c65a33b988f05f
chanho0908 <uni070@naver.com>
2024-11-11 06:59:42 +0900

    feat : 영수증 출력 로직을 위한 UiEvent 추가

--- src/main/kotlin/store/presentation/event/UiEvent.kt
+++ src/main/kotlin/store/presentation/event/UiEvent.kt
@@ -1,10 +1,13 @@
 package store.presentation.event
 
+import store.domain.model.receipt.Receipt
+
 sealed class UiEvent {
     data class Loading(val message: String) : UiEvent()
-    data class UserAccess(val message: String): UiEvent()
+    data class UserAccess(val message: String) : UiEvent()
     data class FinalizeOrder(
         val notReceivedPromotionMsg: List<String>?,
         val shortageStockMsg: List<String>?,
-    ): UiEvent()
+    ) : UiEvent()
+    data class MakeOutReceipt(val receipt: Receipt) : UiEvent()
 }
