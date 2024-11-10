7199996f8ec8a0590059355f45eedc58b452e0c1
chanho0908 <uni070@naver.com>
2024-11-10 23:22:06 +0900

    feat : 사용자가 Y,N을 입력할 때 유효성 검사 수행 확장함수 추가

--- src/main/kotlin/store/domain/ext/StringExt.kt
+++ src/main/kotlin/store/domain/ext/StringExt.kt
@@ -4,6 +4,8 @@ import store.domain.model.Constants
 import store.domain.model.Constants.SQUARE_BRACKETS_LEFT
 import store.domain.model.Constants.SQUARE_BRACKETS_RIGHT
 import store.domain.model.output.OutputRules.STOCK_UNIT
+import store.domain.model.output.OutputRules.NO
+import store.domain.model.output.OutputRules.YES
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -27,4 +29,9 @@ fun String.extractProductQuantity(): String {
     return productQuantity.trim().removeSuffix("$SQUARE_BRACKETS_RIGHT")
 }
 
-fun String.removeStockUnitSuffix(): String = this.removeSuffix("$STOCK_UNIT")fun String.removeNumberFormat():Int = this.replace(",", "").toInt()
+fun String.removeStockUnitSuffix(): String = this.removeSuffix("$STOCK_UNIT")
+
+fun String.removeNumberFormat():Int = this.replace(",", "").toInt()
+
+fun String.isNo(): Boolean = this.equals(NO.toString(), ignoreCase = true)
+fun String.isYes(): Boolean = this.equals(YES.toString(), ignoreCase = true)
